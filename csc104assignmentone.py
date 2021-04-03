a309d149550703de219970709d16dbc88d0c4b91
smbbagudu <81766699+smbbagudu@users.noreply.github.com>
2021-04-03 08:10:44 -0700

    Add files via upload

---
+++ csc104assignmentone.py
@@ -0,0 +1,45 @@
+''' 1. write a short python code to display your name amd department
+'''
+name = 'Mohammed Sani Busu'
+department = 'Computer Scsccience'
+print('Name:',name,"\n"'Department:',department)
+
+''' 2.Assign the string 'boo!' to the variable scare and print the variable
+'''
+scare = 'Boo!'
+print(scare)
+
+''' 3.Add two variable and assign the result to a third varible.
+	print all the varible names 
+'''
+sani_score = 5
+musa_score = 6
+total_scores = sani_score + musa_score
+print(sani_score," ",musa_score," ",total_scores) 
+''' 4. divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable
+'''
+number1 = 12
+number2 = .5
+result = number1 / number2
+print(result)
+
+''' 5. assign the sum of two numbers to a variable and print the variable
+'''
+sum_of_two_numbers = 5 + 5
+print(sum_of_two_numbers)
+
+''' write the code that find a remainder when 8 is divided by 3
+'''
+first_number = 8
+second_number = 3
+the_remainder = first_number % second_number
+print(the_remainder)
+
+''' Rewrite the following statment to force this orders first, multiply 2 by 4. then add 4 and 2
+	then multiply the first result by second result print the result.
+	x = 2*4*4+2
+'''
+first_result = 2*4
+second_result = 4+2
+first_and_second_results = first_result * second_result
+print(first_and_second_results) 
